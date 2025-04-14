6c5920e61b5d0e9307dac81dc56778d08c4b71b7
fredericacode <3827218655@qq.com>
2025-04-14 22:30:25 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Metafalica status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm800305131-844b52b93ab18ee46f6824b5'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'metafalica',
+      url: 'https://at2.jtcat.com/'
+    },
+  ],
+};
